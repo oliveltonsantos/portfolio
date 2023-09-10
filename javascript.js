42a14161ecc962c6a80354b12a9ccb8b10f3d33d
Olivelton Santos <124373744+oliveltonsantos@users.noreply.github.com>
2023-09-10 01:17:23 -0300

    update

--- javascript.js
+++ javascript.js
@@ -1,6 +1,10 @@
-const activePage = window.location.pathname;
-const navLinks = document.querySelectorAll('nav a').forEach(link => {
-    if (link.href.includes(`${activePage}`)) {
-        link.classList.add('active');
+const navLinkEls = document.querySelectorAll('.nav-link');
+const WindowPathname = window.location.pathname;
+
+navLinkEls.forEach(navLinkEl => {
+    const navLinkPathname = new URL(navLinkEl.href).pathname;
+
+    if ((WindowPathname === navLinkPathname) || (WindowPathname === '/index.html' && navLinkPathname === '/')) {
+        navLinkEl.classList.add('active');
     }
 })
