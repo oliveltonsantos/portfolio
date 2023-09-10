d036b1730c607c26b848e715cc7167c017a20d2c
Olivelton Santos <124373744+oliveltonsantos@users.noreply.github.com>
2023-09-10 01:41:19 -0300

    update

--- javascript.js
+++ javascript.js
@@ -2,9 +2,11 @@ const navLinkEls = document.querySelectorAll('.nav-link');
 const WindowPathname = window.location.pathname;
 
 navLinkEls.forEach(navLinkEl => {
+
     const navLinkPathname = new URL(navLinkEl.href).pathname;
 
-    if ((WindowPathname === navLinkPathname) || (WindowPathname === '/index.html' && navLinkPathname === '/')) {
+
+    if ((WindowPathname === navLinkPathname) || (WindowPathname === 'index.html' && navLinkPathname === '/')) {
         navLinkEl.classList.add('active');
     }
 })
