63b6732b7a427806c20934f8ccc3fe4d56624535
Olivelton Santos <124373744+oliveltonsantos@users.noreply.github.com>
2023-09-11 13:50:27 -0300

    update

--- javascript.js
+++ javascript.js
@@ -1,3 +1,24 @@
+/* Menu na versão mobile*/
+
+let btnMenu = document.getElementById('btn-menu')
+let menu = document.getElementById('menu-mobile')
+let overlay = document.getElementById('overlay-menu')
+
+btnMenu.addEventListener('click', () => {
+    menu.classList.add('abrir-menu')
+})
+
+menu.addEventListener('click', () => {
+    menu.classList.remove('abrir-menu')
+})
+
+overlay.addEventListener('click', () => {
+    menu.classList.remove('abrir-menu')
+})
+
+
+/* Link ativo na versão de desktop */
+
 const navLinkEls = document.querySelectorAll('.nav-link');
 const WindowPathname = window.location.pathname;
 
